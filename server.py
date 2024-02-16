f3f29e712d4c0115506333fc4af893768a9ee5e9
Konstantin Dubrouski <kostga123@mail.ru>
2024-02-16 16:40:27 +0300

    server v1 ready

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 import socket
 
-
-from commands import Commands
+from commands import Commands, ResponseCodes
 
 HOST = "127.0.0.1"
 PORT = 65432
@@ -16,24 +17,39 @@ def recv_command(s):
     command_packet_parts = str(command_bytes, 'utf8').split('&')
     return command, command_packet_parts[1:len(command_packet_parts)]
 
-def send_response(s : socket, args : []):
-    message = '&'.join(args)
+def send_response(s : socket, code : ResponseCodes, args : []):
+    message = str(code.value) + '&'.join(args)
     s.send(len(message).to_bytes(4, 'big'))
     s.sendall(bytes(message, encoding='utf8'))
 
-# Создание сокета и отправка имени серверу
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    username = ""
+
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            command, args = recv_command(s)
+    while True:
+        print("Wait for client...")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                command, args = recv_command(s)
 
-            if command == Commands.ECHO.value:
-                send_response(conn, [args[0]])
+                if command == Commands.HI.value:
+                    username = args[0]
+                    send_response(conn, ResponseCodes.SUCCESS, [])
 
-            break
+                if command == Commands.ECHO.value:
+                    send_response(conn, ResponseCodes.SUCCESS, [args[0]])
+
+                if command == Commands.TIME.value:
+                    now = datetime.now().time()
+                    send_response(conn, ResponseCodes.SUCCESS, [str(now.hour), str(now.minute), str(now.second)])
+                if command == Commands.QUIT.value:
+                    send_response(conn, ResponseCodes.SUCCESS, [])
+                    break
+                
+
+            
 
     
