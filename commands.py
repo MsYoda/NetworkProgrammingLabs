e17fb5c5aca40d8680b54f913b851db677983190
ni0x0 <nik.x.i0ta@gmail.com>
2024-02-16 16:12:08 +0300

    more readme

--- commands.py
+++ commands.py
@@ -1,7 +1,33 @@
 import enum
 
 class Commands(enum.Enum):
-    HI = 0      # args: user_name
-    ECHO = 1    # args: text_to_return
-    QUIT = 2    # args: no
+    HI = 0           # args: user_name
+    ECHO = 1         # args: text_to_return
+    QUIT = 2         # args: no
+    TIME = 3         # args: no
+    UPLOAD = 4       # args: file_name file_size(скрытый)
+    DOWNLOAD = 5     # args: file_name
+    LIST = 6         # args: no
 
+class ResponseCodes(enum.Enum):
+    SUCCESS = 200
+    UNFINISHED = 201   # нужны доп. действия
+    ERROR = 400
+
+
+"""
+    Пример взаимодействия
+    - ECHO текст текст -> разделить и отправить
+    - клиент ждет ответ (набор байт, что преобразуются в строку)
+    - 200&текст текст
+
+    
+    UPLOAD
+        UPLOAD cat.jpg (UPLOAD cat.jpg размер_файла(строкой))- разбить на команду-обьект класса, лист строк
+        отправить
+        клиент получает "200" -> клиент шлет по 64кб -> ввод пользователя
+                        "400" -> вывод ошибки
+
+    
+"""
+    
