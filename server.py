5bca75cb8df3d9fcd22ad2ec10046b6f901bc2d4
Konstantin Dubrouski <kostga123@mail.ru>
2024-02-19 11:28:19 +0300

    add error handling on server

--- server.py
+++ server.py
@@ -12,12 +12,12 @@ print("Hello! It a BSUIR network file manager server")
 
 def recv_command(s):
     length = conn.recv(4)
-    if (len(length) == 0): raise Exception()
+    if (len(length) == 0): raise ConnectionError('connection lost')
 
     command_len = int.from_bytes(length, 'big')
 
     command_bytes = conn.recv(command_len)
-    if (len(command_bytes) == 0): raise Exception()
+    if (len(command_bytes) == 0): raise ConnectionError('connection lost')
 
     command = command_bytes[0]
 
@@ -28,6 +28,42 @@ def send_response(s : socket, code : ResponseCodes, args : []):
     message = str(code.value) + '&' + '&'.join(args)
     s.send(len(message).to_bytes(4, 'big'))
     s.sendall(bytes(message, encoding='utf8'))
+
+def send_file(s, filename, filesize, offset = 0):
+    with open(filename, "rb") as file:
+        file.seek(offset)
+        buffer_size = 64 * 1024
+        proccesed_bytes = offset
+        while True:
+            data = file.read(buffer_size)
+            if len(data) == 0: break
+
+            sended = conn.send(data)
+            if sended == 0: break
+            proccesed_bytes = proccesed_bytes + sended
+
+            percentage = (proccesed_bytes / file_size) * 100
+            print(f'{percentage:.2f}%')
+
+        recv_command(conn)
+
+def recv_file(s, filename, file_size, offset = 0):
+    mode = 'wb'
+    if offset > 0: mode = 'ab'
+    proccesed_bytes = offset
+
+    with open(filename, mode) as file:
+        buffer_size = 64 * 1024
+        while proccesed_bytes < file_size:
+            data = conn.recv(buffer_size)
+            if (len(data) == 0): 
+                file.close()
+                raise ConnectionError('connection lost')
+            proccesed_bytes = proccesed_bytes + len(data)
+            file.write(data)
+
+            percentage = (proccesed_bytes / file_size) * 100
+            print(f'{percentage:.2f}%')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     username = ""
@@ -47,124 +83,158 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 args = []
                 try: 
                     command, args = recv_command(s)
-                except:
+                except ValueError as e:
+                    print(f'{username} caused {e} when recv_command')
+                    send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                except ConnectionError as e:
+                    print(f'{username} caused {e} when recv_command')
+                    break 
+                except Exception as e:
+                    print(f'{username} caused {e} when when recv_command') 
                     break
 
                 if command == Commands.HI.value:
                     if args[0] == username and file_size != 0:
                         if isUpload:
-                            proccesed_bytes = os.path.getsize('server_files/' + filename)
-                            send_response(conn, ResponseCodes.UNFINISHED_UP, [filename, str(proccesed_bytes)])
                             try:
-                                with open('server_files/' + filename, 'ab') as file:
-                                    buffer_size = 64 * 1024
-                                    while proccesed_bytes < file_size:
-                                        data = conn.recv(buffer_size)
-                                        if (len(data) == 0): 
-                                            file.close()
-                                            raise Exception()
-                                        proccesed_bytes = proccesed_bytes + len(data)
-                                        file.write(data)
+                                proccesed_bytes = os.path.getsize('server_files/' + filename)
+                                send_response(conn, ResponseCodes.UNFINISHED_UP, [filename, str(proccesed_bytes)])
+
+                                recv_file(conn, 'server_files/' + filename, file_size, proccesed_bytes)
+
                                 filename = ''
                                 file_size = 0
                                 isUpload = False
-                            except:
-                                print("File dont UPLOADed with success")
-                        else:
-                            send_response(conn, ResponseCodes.UNFINISHED_DOWN, [filename, str(os.path.getsize('server_files/' + filename)), str(64 * 1024)])
-                            code, args = recv_command(conn)
-                            proccesed_bytes = int(args[0])
-                            try:
-                                with open('server_files/' + filename, "rb") as file:
-                                    file.seek(proccesed_bytes)
-                                    buffer_size = 64 * 1024
-                                    while True:
-                                        data = file.read(buffer_size)
-                                        if len(data) == 0: break
-                                        conn.send(data)
-                                        print("Server downlaod send")
-                                recv_command(conn)
+                            except (FileNotFoundError, FileExistsError) as e:
+                                print(f'{username} caused {e} when retry to upload {filename}')
+                                send_response(conn, ResponseCodes.ERROR, [filename, 'dont exsist'])
                                 filename = ''
                                 file_size = 0
-                            except:
-                                print("Downlaod error")
+                                isUpload = False
+                            except (ValueError, IndexError) as e:
+                                print(f'{username} caused {e} when retry to upload {filename}')
+                                send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                                filename = ''
+                                file_size = 0
+                                isUpload = False
+                            except ConnectionError as e:
+                                print(f'{username} caused {e} when retry to upload {filename}') 
+                            except Exception as e:
+                                print(f'{username} caused {e} when retry to upload {filename}') 
+                        else:
+                            try:
+                                send_response(conn, ResponseCodes.UNFINISHED_DOWN, [filename, str(os.path.getsize('server_files/' + filename)), str(64 * 1024)])
+                                code, args = recv_command(conn)
+                                proccesed_bytes = int(args[0])
 
+                                send_file(conn, 'server_files/' + filename, file_size, proccesed_bytes)
+
+                                filename = ''
+                                file_size = 0
+                            except (FileNotFoundError, FileExistsError) as e:
+                                print(f'{username} caused {e} when retry to download {filename}')
+                                send_response(conn, ResponseCodes.ERROR, [filename, 'dont exsist'])
+                                filename = ''
+                                file_size = 0
+                            except (ValueError, IndexError) as e:
+                                print(f'{username} caused {e} when retry to download {filename}')
+                                send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                                filename = ''
+                                file_size = 0
+                            except ConnectionError as e:
+                                print(f'{username} caused {e} when retry to download {filename}') 
+                            except Exception as e:
+                                print(f'{username} caused {e} when retry to download {filename}') 
                     else:
-                        username = args[0]
-                        send_response(conn, ResponseCodes.SUCCESS, []) 
+                        try:
+                            username = args[0]
+                            file_size = 0
+                            filename = ''
+                            send_response(conn, ResponseCodes.SUCCESS, []) 
+                        except IndexError as e:
+                            print(f'{username} caused {e} when retry to download {filename}')
+                            send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                            file_size = 0
+                            filename = ''
+
 
                 if command == Commands.ECHO.value:
-                    send_response(conn, ResponseCodes.SUCCESS, [args[0]])
+                    try:
+                        print(f'ECHO from {username}: {args[0]}')
+                        send_response(conn, ResponseCodes.SUCCESS, [args[0]])
+                    except IndexError as e:
+                        print(f'{username} caused {e} when retry to download {filename}')
+                        send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
 
                 if command == Commands.TIME.value:
+                    print(f'TIME from {username}')
                     now = datetime.now().time()
                     send_response(conn, ResponseCodes.SUCCESS, [str(now.hour), str(now.minute), str(now.second)])
                     
                 if command == Commands.QUIT.value:
+                    print(f'QUIT from {username}')
                     send_response(conn, ResponseCodes.SUCCESS, [])
                     break
 
                 if command == Commands.LIST.value:
+                    print(f'LIST from {username}')
                     files = []
                     for file in os.listdir('server_files/'):
                         files.append(file)
                     send_response(conn, ResponseCodes.SUCCESS, files)
 
                 if command == Commands.DOWNLOAD.value:
-                    filename = args[0]
-                    file_size = os.path.getsize(filename)
-                    proccesed_bytes = 0
                     try:
-                        with open('server_files/' + filename, "rb") as file:
-                            buffer_size = 64 * 1024
-                            send_response(conn, ResponseCodes.SUCCESS, [str(file_size), str(buffer_size)])
-                            while True:
-                                data = file.read(buffer_size)
-                                if len(data) == 0: break
-                                sended = conn.send(data)
-                                if (sended == 0): 
-                                    file.close()
-                                    raise Exception
-                            recv_command(conn)
-                            filename = ''
-                            file_size = 0
+                        filename = args[0]
+                        file_size = os.path.getsize(filename)
 
-                    except:
-                        print("Downlaod error")
+                        print(f'DOWNLOAD {filename} from {username}')
+
+                        send_response(conn, ResponseCodes.SUCCESS, [str(file_size), str(64 * 1024)])
+                        send_file(conn, 'server_files/' + filename, file_size)
+                        filename = ''
+                        file_size = 0
+                    except (FileNotFoundError, FileExistsError) as e:
+                        print(f'{username} caused {e} when try to download {filename}')
+                        send_response(conn, ResponseCodes.ERROR, [filename, 'dont exsist'])
+                        filename = ''
+                        file_size = 0
+                    except (ValueError, IndexError) as e:
+                        print(f'{username} caused {e} when try to download {filename}')
+                        send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                        filename = ''
+                        file_size = 0
+                    except ConnectionError as e:
+                        print(f'{username} caused {e} when try to download {filename}') 
+                    except Exception as e:
+                        print(f'{username} caused {e} when try to download {filename}') 
 
                 if command == Commands.UPLOAD.value:
-                    filename = args[0]
-                    file_size = int(args[1])
-                    proccesed_bytes = 0
-                    isUpload = True
                     try:
-                        with open('server_files/' + filename, 'wb') as file:
-                            buffer_size = 64 * 1024
-                            proccesed_bytes = 0
-                            send_response(conn, ResponseCodes.SUCCESS, [])
-                            while proccesed_bytes < file_size:
-                                data = conn.recv(buffer_size)
-                                if (len(data) == 0): 
-                                    file.close()
-                                    raise Exception()
-                                proccesed_bytes = proccesed_bytes + len(data)
-                                file.write(data)
+                        filename = args[0]
+                        file_size = int(args[1])
+                        isUpload = True
+
+                        print(f'UPLOAD {filename} from {username}')
+                        
+                        send_response(conn, ResponseCodes.SUCCESS, [])
+                        recv_file(conn, 'server_files/' + filename, file_size)
                         filename = ''
                         file_size = 0
                         isUpload = False
-                    except:
-                        print("File dont UPLOADed with success")
-
-
-
-
-
-
-                       
-                    
-
-                
-
-            
-
-    
+                    except (FileNotFoundError, FileExistsError) as e:
+                        print(f'{username} caused {e} when try to upload {filename}')
+                        send_response(conn, ResponseCodes.ERROR, [filename, 'dont exsist'])
+                        filename = ''
+                        file_size = 0
+                        isUpload = False
+                    except (ValueError, IndexError) as e:
+                        print(f'{username} caused {e} when try to upload {filename}')
+                        send_response(conn, ResponseCodes.ERROR, ['bad arguments'])
+                        filename = ''
+                        file_size = 0
+                        isUpload = False
+                    except ConnectionError as e:
+                        print(f'{username} caused {e} when try to upload {filename}') 
+                    except Exception as e:
+                        print(f'{username} caused {e} when try to upload {filename}') 
